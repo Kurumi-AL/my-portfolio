51faa51703be7ed956ac252c3752c66949e54764
Kurumi Lawrence <rinnrinn292006@gmail.com>
2022-07-29 11:54:52 -0500

    changes

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -5,13 +5,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC1h4ZUn4Xdmn3edGdN2FLtbQc2W9kSufE",
-  authDomain: "my-portfolio-f4066.firebaseapp.com",
-  projectId: "my-portfolio-f4066",
-  storageBucket: "my-portfolio-f4066.appspot.com",
-  messagingSenderId: "479687261867",
-  appId: "1:479687261867:web:39378e99ed748ef5df01ee",
-  measurementId: "G-NNY8BM37B4",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
